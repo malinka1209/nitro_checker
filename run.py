202d39b8339205227506fb40e7d36f748f68347b
malinka1209 <malinka.2012@internet.ru>
2026-05-27 20:46:44 +0300

    Nitro bot for Bothost

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python3
-from dotenv import load_dotenv
 import os
-
-# Загружаем переменные из .env файла
-load_dotenv()
-
-# Запускаем бота
 from nitro_bot import main
 
 if __name__ == "__main__":
